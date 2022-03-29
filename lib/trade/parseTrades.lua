c8aad183566072679a7c10c0484447b3b0d90681
Zach Lott <zacharylott94@gmail.com>
2022-03-28 19:48:05 -0500

    Had a broken pattern

--- lib/trade/parseTrades.lua
+++ lib/trade/parseTrades.lua
@@ -14,7 +14,7 @@ end
 local function parse(line)
   local trade = {}
   --separate line into itemname,quantity substrings
-  for each in string.gmatch(line, "(%w+:%w,%d+)") do
+  for each in string.gmatch(line, "(%w+:%w+,%d+)") do
     -- parse each substring into an Item table
     local item = parseItem(each)
     table.insert(trade, item)
