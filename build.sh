b0b2d2fc39550b72bc03001f33be32dd217a77e6
Zach Lott <zacharylott94@gmail.com>
2022-03-29 22:14:11 -0500

    switched to a dev branch

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 LUA_PATH="./lib/?.lua;./lib/list/?.lua;;" amalg.lua -o ./portable/list.lua  list map fold filter
-LUA_PATH="./lib/?.lua;./lib/trade/?.lua;./portable/?.lua;;" amalg.lua -o ./portable/emptySlots.lua emptySlots id list
+LUA_PATH="./lib/?.lua;./lib/trade/?.lua;./portable/?.lua;;" amalg.lua -d -o ./portable/emptySlots.lua emptySlots id list
 #luacc -o ./portable/emptySlots.lua -i ./portable list -i ./lib/trade/ emptySlots -i ./lib id  
--- portable/emptySlots.lua
+++ portable/emptySlots.lua
@@ -1,35 +1,20 @@
-do
-local _ENV = _ENV
-package.preload[ "emptySlots" ] = function( ... ) local arg = _G.arg;
-local list = require("list")
-local id = require("id")
+package.preload[ "emptySlots" ] = assert( (loadstring or load)( "local list = require(\"list\")\
+local id = require(\"id\")\
+\
+local filter = list.filter(id)\
+\
+return function(inventory)\
+  return inventory.size() - filter(inventory)\
+end", '@'.."./lib/trade/emptySlots.lua" ) )
 
-local filter = list.filter(id)
+package.preload[ "id" ] = assert( (loadstring or load)( "return function(i) return i end end", '@'.."./lib/id.lua" ) )
 
-return function(inventory)
-  return inventory.size() - filter(inventory)
-end
-end
-end
-
-do
-local _ENV = _ENV
-package.preload[ "id" ] = function( ... ) local arg = _G.arg;
-return function(i) return i end end
-end
-end
-
-do
-local _ENV = _ENV
-package.preload[ "list" ] = function( ... ) local arg = _G.arg;
--- while these will work with any table, they don't make sense in context.
--- This is strictly for tables that act as lists
-local list = {}
-list.fold = require("./list/fold")
-list.map = require("./list/map")
-list.filter = require("./list/filter")
-
-return list
-end
-end
+package.preload[ "list" ] = assert( (loadstring or load)( "-- while these will work with any table, they don't make sense in context.\
+-- This is strictly for tables that act as lists\
+local list = {}\
+list.fold = require(\"./list/fold\")\
+list.map = require(\"./list/map\")\
+list.filter = require(\"./list/filter\")\
+\
+return list", '@'.."./lib/list.lua" ) )
 
