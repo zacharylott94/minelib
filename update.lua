e3f212948b101da7e7f9d1c3ec7cb9017b166209
Zach Lott <zacharylott94@gmail.com>
2022-04-04 08:33:17 -0500

    made an update script to easily pull down changes

---
+++ update.lua
@@ -0,0 +1,4 @@
+-- https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/
+curl curl.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/curl.lua
+curl emptySlots.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/emptySlots.lua
+curl getItemCount.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/getItemCount.lua
