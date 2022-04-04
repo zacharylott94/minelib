092a4994def0d22b38b3d424ae05e64a0a760b4f
Zach Lott <zacharylott94@gmail.com>
2022-04-04 08:47:45 -0500

    fixed update script

--- update.lua
+++ update.lua
@@ -1,4 +1,4 @@
 -- https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/
-curl curl.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/curl.lua
-curl emptySlots.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/emptySlots.lua
-curl getItemCount.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/getItemCount.lua
+shell.run("curl curl.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/curl.lua")
+shell.run("curl emptySlots.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/emptySlots.lua")
+shell.run("curl getItemCount.lua https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/getItemCount.lua")
