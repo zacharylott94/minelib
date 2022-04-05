1d7f347ddd83987c78690c8f075039597e9af1f5
Zach Lott <zacharylott94@gmail.com>
2022-04-05 12:51:11 -0500

    If this works, refactor it

--- lib/trade/router.lua
+++ lib/trade/router.lua
@@ -28,7 +28,12 @@ local routeOperation = function(route)
   local sourceItems = gic(route.item)(route.source.list())
   local destinationItems = gic(route.item)(route.destination.list())
   local sourceDelta = sourceItems - route.reserve
-  local destinationDelta = route.limit - destinationItems
+  local destinationDelta 
+  if (route.limit == 0) then
+    destinationDelta = 1
+  else
+    destinationDelta = route.limit - destinationItems
+  end
   local firstAvailableSlot = itemSlots(route.item,route.source.list())[1]
   if (sourceDelta > 0) and (destinationDelta > 0) then
     local amount
@@ -38,13 +43,18 @@ local routeOperation = function(route)
       amount = sourceDelta
     end
       route.source.pushItems(peripheral.getName(route.destination),firstAvailableSlot,amount)
+    return true, amount
   end
-
+  return false
 end
 
 while true do
   for _,v in pairs(routes) do
-    routeOperation(v)
+    local success, amount = routeOperation(v)
+    if success then
+      local destination = peripheral.getName(v.destination)
+      local source = peripheral.getName(v.source)
+      print(string.format("Pushed %s %s from %s to %s", amount, v.item, source, destination))
   end
 
 end
