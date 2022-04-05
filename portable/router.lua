ac749237336dc4214eba63703caa2ad7211fe753
Zach Lott <zacharylott94@gmail.com>
2022-04-05 12:21:19 -0500

    trying to add a destination limit That way, the router won't flood a destination

--- portable/router.lua
+++ portable/router.lua
@@ -467,32 +467,37 @@ local csv = require("csv")
 
 local routerTable = io.open("routertable", "r")
 
--- {destination,source,item,reserve}[]
+-- {destination,source,item,reserve,limit}[]
 local routes = csv.parseh(routerTable)
 
-local wrapSource = function (route) 
+local wrapPeripherals = function (route) 
   route.source = peripheral.wrap(route.source)
+  route.destination = peripheral.wrap(route.destination)
   return route
 end
 
-local reserveAsNumber = function (route)
+local convertNumbers = function (route)
   route.reserve = tonumber(route.reserve)
+  route.limit = tonumber(route.limit)
   return route
 end
 local list = require("list")
 local h = require("lambda")
 
-routes = list.map(h.compose(wrapSource, reserveAsNumber))(routes)
+routes = list.map(h.compose(wrapPeripherals, convertNumbers))(routes)
 
 local gic = require("getItemCount")
 local itemSlots = require("itemSlots")
 
 local routeOperation = function(route)
-  local totalItems = gic(route.item)(route.source.list())
-  local difference = totalItems - route.reserve
+  local sourceItems = gic(route.item)(route.source.list())
+  local destinationItems = gic(route.item)(route.destination.list())
+  local sourceDelta = sourceItems - route.reserve
+  local destinationDelta = route.limit - destinationItems
   local firstAvailableSlot = itemSlots(route.item,route.source.list())[1]
-  if difference > 0 then
-    route.source.pushItems(route.destination,firstAvailableSlot,difference)
+  if (sourceDelta > 0) and (destinationDelta > 0) then
+    let amount = math.min(sourceDelta, destinationDelta)
+    route.source.pushItems(peripheral.getName(route.destination),firstAvailableSlot,amount)
   end
 
 end
