a52601962b98e9aa98c8696d31a1a9914bb97c72
Zach Lott <zacharylott94@gmail.com>
2022-04-05 12:38:33 -0500

    should be able to not have a limit

--- portable/router.lua
+++ portable/router.lua
@@ -496,8 +496,13 @@ local routeOperation = function(route)
   local destinationDelta = route.limit - destinationItems
   local firstAvailableSlot = itemSlots(route.item,route.source.list())[1]
   if (sourceDelta > 0) and (destinationDelta > 0) then
-    local amount = math.min(sourceDelta, destinationDelta)
-    route.source.pushItems(peripheral.getName(route.destination),firstAvailableSlot,amount)
+    local amount
+    if (route.limit > 0) then
+      amount = math.min(sourceDelta, destinationDelta)
+    else
+      amount = sourceDelta
+    end
+      route.source.pushItems(peripheral.getName(route.destination),firstAvailableSlot,amount)
   end
 
 end
