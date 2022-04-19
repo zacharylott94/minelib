ff9147b0918ad20dfc07926a9365a27332e0b0fc
Zach Lott <zacharylott94@gmail.com>
2022-04-19 14:59:34 -0500

    typo in script

--- portable/installation/installRouter.lua
+++ portable/installation/installRouter.lua
@@ -1,5 +1,5 @@
 shell.run("wget https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/installation/routeraliases")
-shell.run("wget https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/installation/routeratable")
+shell.run("wget https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/installation/routertable")
 shell.run("wget https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/updater_scripts/updateRouter.lua")
 shell.run("wget https://raw.githubusercontent.com/zacharylott94/minelib/master/portable/updater_scripts/updateRouter_dev.lua")
 shell.run("updateRouter")
